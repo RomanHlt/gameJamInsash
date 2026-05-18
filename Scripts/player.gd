50b3e936f38c88e23c72ea434a2f33d812d226e8
RomanHlt <romanhalatre@gmail.com>
2026-05-18 14:32:01 +0200

    map

---
+++ Scripts/player.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name Player
+
+var posX:int = 0;
+var posY:int = 0;
+var viewFrot:bool = true; 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
