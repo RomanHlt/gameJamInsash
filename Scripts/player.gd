73a10e32d68fa5383d8af33f49a626f7ffb94bee
RomanHlt <romanhalatre@gmail.com>
2026-05-20 12:52:15 +0200

    Player assets

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,12 +4,13 @@ class_name Player
 @export var posX:int = 0;
 @export var posY:int = 0;
 @export var viewFront:bool = true; 
-
+@export var wallSize:int = 16
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+func _process(delta: float) -> void:
+	estimate_pos()
+
+func estimate_pos() -> void:
+	posX = int(position.x/wallSize)
